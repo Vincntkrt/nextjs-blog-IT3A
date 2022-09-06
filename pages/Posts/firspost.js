0fe893905b52b4c66bc326787cf9ec327b81382b
Vincent <vkigonzales.student@ua.edu.ph>
2022-09-06 14:46:16 +0800

    first commit

---
+++ pages/Posts/firspost.js
@@ -0,0 +1,96 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title> Self Introduction </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <img src="/gggg.jpg" alt="Vincent" className="logo" />
+
+      <main>
+        <h1 className="title">
+          Gonzales, Vincent Kurt I. <a href="https://nextjs.org"></a>
+        </h1>
+
+        <p className="description">
+          My name is vincent gonzales, 19 yrs old i live in macabebe pampanga and my hobbies are playing online games and watching movies. my favorit food is beef brocoli.
+        </p>
+
+        <div className="grid">
+          <a href="https://nextjs.org/docs
+
+" className="card">
+          
+           
+          </a>
+          <a>
+          </a>
+
+        </div>
+      </main>
+    
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+        .logo {
+          border-radius: 700px;
+          background-size: 50px;
+          width: 300px;
+          height: 300px;
+          margin-top: 75px;    
+        }
+        
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
